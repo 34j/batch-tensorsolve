b9d47241e840ffcd73459f78a713138c437fc576
34j <34j.github@proton.me>
2025-05-10 11:19:32 +0900

    fix: export broadcast_without_repeating

--- src/batch_tensorsolve/__init__.py
+++ src/batch_tensorsolve/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.0.0"
-from ._main import AmbiguousBatchAxesWarning, btensorsolve
+from ._main import AmbiguousBatchAxesWarning, broadcast_without_repeating, btensorsolve
 
-__all__ = ["AmbiguousBatchAxesWarning", "btensorsolve"]
+__all__ = ["AmbiguousBatchAxesWarning", "broadcast_without_repeating", "btensorsolve"]
