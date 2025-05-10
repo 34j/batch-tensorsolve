924a8b99a99e50e01018a4848bf05e95acbcf1f6
34j <55338215+34j@users.noreply.github.com>
2025-05-10 11:30:35 +0900

    feat: add main feat (#4)
    * feat: add main feat
    
    * fix: fix warnings not warned
    
    * docs: better docs
    
    * docs: better docs
    
    * fix: export broadcast_without_repeating
    
    * fix: export broadcast_without_repeating
    
    * test: add more tests

--- src/batch_tensorsolve/__init__.py
+++ src/batch_tensorsolve/__init__.py
@@ -1,1 +1,4 @@
 __version__ = "0.0.0"
+from ._main import AmbiguousBatchAxesWarning, broadcast_without_repeating, btensorsolve
+
+__all__ = ["AmbiguousBatchAxesWarning", "broadcast_without_repeating", "btensorsolve"]
